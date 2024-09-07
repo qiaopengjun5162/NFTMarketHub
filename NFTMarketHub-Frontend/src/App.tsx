6c9a488937c996573d698b2e693cd1f5f646ab04
QiaoPengjun5162 <qiaopengjun0@gmail.com>
2024-09-07 10:58:55 +0800

    feat:  NFTMarketHub-Frontend init

--- NFTMarketHub-Frontend/src/App.tsx
+++ NFTMarketHub-Frontend/src/App.tsx
@@ -1,21 +1,24 @@
-import { ConnectButton } from "@rainbow-me/rainbowkit";
+import Layout from "@components/Layout";
+import HomePage from "@pages/HomePage";
+import ERC20Page from "@pages/ERC20Page";
+import NFTPage from "@pages/NFTPage";
+import MarketPage from "@pages/MarketPage";
+import { Route, Routes } from "react-router-dom";
+
 
 const App = () => {
-  // const { address, isConnected } = useAccount();
 
   return (
-    <div className="relative min-h-screen p-4">
-      {/* 
-        mx-auto:用于水平居中元素。mx 表示 margin-left 和 margin-right，auto 使这些边距自动，以实现水平居中对齐。
-        flex:将元素的布局模式设置为 Flexbox。这使得元素可以使用 Flexbox 的布局特性，如对齐、分布等。
-      */}
-      <nav className="container mx-auto px-4 py-8 flex items-center justify-between">
-        <h1 className="text-3xl font-bold">Welcome to NFTMarketHub</h1>
-        <div className="absolute top-4 right-4">
-          <ConnectButton />
-        </div>
-      </nav>
-    </div>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/erc20" element={<ERC20Page />} />
+        <Route path="/nft" element={<NFTPage />} />
+        <Route path="/market" element={<MarketPage />} />
+        <Route path="*" element={<h1>404 NOT FOUND</h1>} />
+
+      </Routes>
+    </Layout>
   );
 };
 
--- NFTMarketHub-Frontend/src/vite-env.d.ts
+++ NFTMarketHub-Frontend/src/vite-env.d.ts
@@ -1,5 +1,7 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
-    readonly VITE_API_URL: string;
-    readonly VITE_API_KEY: string;
+    readonly VITE_PUBLIC_ENABLE_TESTNETS: string;
+    readonly VITE_NFTMarket_Contract_Address: string;
+    readonly VITE_MyERC20Token_Contract_Address: string;
+    readonly VITE_MyERC721Token_Contract_Address: string;
 }
